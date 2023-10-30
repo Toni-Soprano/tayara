e47edca07983acb808408dc77558294c9899511c
omar <ajimiomar.oa@gmail.com>
2023-10-30 13:53:59 +0100

    product added

---
+++ Frontend/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../components/banner'
+import Product from '../components/product'
+const Home = () => {
+    return (
+        <div>
+            <Banner />
+            <Product />
+        </div>
+    )
+}
+export default Home
